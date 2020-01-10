753eaa98a3cd6b43b730a597b50f4fbfc66e8160
Franck <patouillardfranck3@gmail.com>
2020-01-11 00:45:32 +0100

    Pare feu security mis en place

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class AdminController extends AbstractController
 {
     /**
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_SUPER_ADMIN')")
      * @Route("/admin", name="admin")
      */
     public function index()
